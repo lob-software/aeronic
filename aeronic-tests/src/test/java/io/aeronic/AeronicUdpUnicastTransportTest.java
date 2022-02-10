d7f30e4b814ddea49c2267a3992605ae4574fa62
eliquinox <eliquinox@outlook.com>
2022-02-10 16:29:55 +0000

    Introduce AeronicUdpUnicastTransportTest

---
+++ aeronic-tests/src/test/java/io/aeronic/AeronicUdpUnicastTransportTest.java
@@ -0,0 +1,27 @@
+package io.aeronic;
+
+import io.aeron.ChannelUriStringBuilder;
+
+public class AeronicUdpUnicastTransportTest extends AeronicTransportTestBase
+{
+
+    // some test cases do not make sense in the real world but pass by virtue of being run locally
+
+    public static final String UNICAST_CHANNEL = new ChannelUriStringBuilder()
+        .media("udp")
+        .reliable(true)
+        .endpoint("localhost:40457")
+        .build();
+
+    @Override
+    public String getPublicationChannel()
+    {
+        return UNICAST_CHANNEL;
+    }
+
+    @Override
+    public String getSubscriptionChannel()
+    {
+        return UNICAST_CHANNEL;
+    }
+}
