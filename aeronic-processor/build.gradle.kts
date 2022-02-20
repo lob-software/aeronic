7c704d99cc066c897b4aacd72c5b59fbbf70cdb6
eliquinox <eliquinox@outlook.com>
2022-02-20 10:06:04 +0000

    Upgrade assertJ

--- aeronic-processor/build.gradle.kts
+++ aeronic-processor/build.gradle.kts
@@ -15,7 +15,7 @@ dependencies {
     implementation("io.aeron:aeron-all:1.37.0")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
-    testImplementation("org.assertj:assertj-core:3.17.2")
+    testImplementation("org.assertj:assertj-core:3.22.0")
 }
 
 tasks.getByName<Test>("test") {
