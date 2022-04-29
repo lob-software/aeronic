845c2db668e70e62e6ecf5f58b865f0765b24785
eliquinox <eliquinox@outlook.com>
2022-04-29 21:37:16 +0100

    Bump aeron to 1.38.1

--- aeronic-processor/build.gradle.kts
+++ aeronic-processor/build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 dependencies {
     annotationProcessor("com.google.auto.service:auto-service:1.0.1")
     implementation("com.google.auto.service:auto-service:1.0.1")
-    implementation("io.aeron:aeron-all:1.37.0")
+    implementation("io.aeron:aeron-all:1.38.1")
     testImplementation(project(":aeronic-tests"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
