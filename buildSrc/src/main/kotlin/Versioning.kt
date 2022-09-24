2b3945dd1e1a2b3cfc93acf595c980cde479184f
eliquinox <eliquinox@outlook.com>
2022-09-24 15:18:15 +0100

    Bump aeron version

--- buildSrc/src/main/kotlin/Versioning.kt
+++ buildSrc/src/main/kotlin/Versioning.kt
@@ -2,7 +2,7 @@ const val VERSION = "0.0.18"
 
 object Versions
 {
-    const val aeron = "io.aeron:aeron-all:1.38.1"
+    const val aeron = "io.aeron:aeron-all:1.39.0"
     const val aeronic = "io.aeronic:aeronic:${VERSION}"
     const val assertJ = "org.assertj:assertj-core:3.23.1"
     const val awaitility = "org.awaitility:awaitility:4.2.0"
