60475e454c3bd6e835b4abf235ee26736b3d17d7
Eduard Silantyev <eduard.silantyev@weareadaptive.com>
2024-03-12 13:06:25 +0000

    bump aeron to 1.43.0

--- buildSrc/src/main/kotlin/Versioning.kt
+++ buildSrc/src/main/kotlin/Versioning.kt
@@ -2,7 +2,7 @@ const val VERSION = "0.0.18"
 
 object Versions
 {
-    const val aeron = "io.aeron:aeron-all:1.40.0"
+    const val aeron = "io.aeron:aeron-all:1.43.0"
     const val aeronic = "io.aeronic:aeronic:${VERSION}"
     const val assertJ = "org.assertj:assertj-core:3.23.1"
     const val awaitility = "org.awaitility:awaitility:4.2.0"
