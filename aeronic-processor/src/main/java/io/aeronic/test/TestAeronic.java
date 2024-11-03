62d494975a337a10c1aca11ce057b971a218f156
ed <silantyeved@gmail.com>
2024-11-02 23:44:04 -0400

    cleanup

--- aeronic-processor/src/main/java/io/aeronic/test/TestAeronic.java
+++ aeronic-processor/src/main/java/io/aeronic/test/TestAeronic.java
@@ -13,17 +13,19 @@ public class TestAeronic implements Aeronic {
 
     @Override
     @SuppressWarnings("unchecked")
-    public <T> T createPublisher(final Class<T> clazz, final String channel, final int streamId) {
+    public <T> T createPublisher(final Class<T> clazz, final String channel, final int streamId)
+    {
         final AbstractSubscriberInvoker<?> subscriberInvoker = clazzToSubscriberMap.get(clazz);
-        if (subscriberInvoker == null)
-        {
-            throw new IllegalStateException("Cannot create a publisher before registering a subscriber! Register %s subscriber first.".formatted(clazz.getName()));
+        if (subscriberInvoker == null) {
+            throw new IllegalStateException(("Cannot create a publisher before registering a subscriber! " +
+                    "Register %s subscriber first.").formatted(clazz.getName()));
         }
         return (T) subscriberInvoker.getSubscriber();
     }
 
     @Override
-    public <T> void registerSubscriber(final Class<T> clazz, final T subscriberImplementation, final String channel, final int streamId) {
+    public <T> void registerSubscriber(final Class<T> clazz, final T subscriberImplementation, final String channel, final int streamId)
+    {
         clazzToSubscriberMap.put(clazz, createSubscriberInvoker(clazz, subscriberImplementation));
     }
 }
